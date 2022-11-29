52a0d7230b57471ede9abd906b7c1fc208b2abc1
Ksenia Popova <foogle80@gmail.com>
2022-11-29 23:32:57 +0300

    first commit

---
+++ ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите два числа");
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+if (a>b)
+{
+    Console.WriteLine($"Максимальное число = {a}, минимальное число = {b}");
+}
+
+else
+{
+    Console.WriteLine($"Максимальное число = {b}, минимальное число = {a}");
+}
---
+++ ex2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+
+Console.WriteLine("Введите три числа");
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine($"Максимальное число = {a}");
+    }
+
+    else
+    {
+        Console.WriteLine($"Максимальное число = {c}");
+        }
+}
+
+else
+{
+    if (b > c)
+    {
+        Console.WriteLine($"Максимальное число = {b}");
+    }
+
+    else
+    {
+        Console.WriteLine($"Максимальное число = {c}");
+    }
+
+}
